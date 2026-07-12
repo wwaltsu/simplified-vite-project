8a41712cc641288bcd6d677efc3316232a0934aa
wwaltsu <waltsumarcus@gmail.com>
2026-07-13 01:28:08 +0300

    feat: configure prettier

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+export default {
+  printWidth: 120,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'none',
+  semi: false,
+  singleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  jsxSingleQuote: false,
+  bracketSameLine: false,
+  endOfLine: 'lf'
+}
